aab9dd5177e81191077e7abeade28077f08c2318
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-02-09 15:16:07 +0100

    sort by name

--- Project-ReactBasics-Store/src/App.js
+++ Project-ReactBasics-Store/src/App.js
@@ -6,10 +6,16 @@ import Cart from './Components/ShoppingCart/Cart/Cart';
 import Items from './Components/ShoppingCart/Items/Items';
 import { productList } from './assents/productList';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 
 
  function App() {
+   const [minFilter, setMinFilter] = useState(0)
+   const [maxFilter, setMaxFilter] = useState(0)
+   const [searchFilter, setSearchFilter] = useState("")
+   const[amount, setAmount] = useState(0)
+   const[cart, setCart] = useState([])
 
   return (
     <>
--- Project-ReactBasics-Store/src/Components/ProductList/Home/Home.js
+++ Project-ReactBasics-Store/src/Components/ProductList/Home/Home.js
@@ -1,19 +1,32 @@
 import { HomeHeadStyle, HomeMainStyled } from "./HomeStyle"
 import ProductCard from "../ProductCard/ProductCard"
 import { HomeProductsListStyled } from "./HomeStyle"
+import { useState } from "react"
 
 
 
 const Home = ({productList}) => {
+    const [ordination, setOrdination] = useState("")
+
+    const handleSelectOrdination = (event) => setOrdination(event.target.value )
 
     return (
         <HomeMainStyled>
             <HomeHeadStyle>
                 <p>quantidade:{productList.length}</p>
-                <p>INPUT</p>
+                <div>
+                    <select name="" id="" value={ordination} onChange={handleSelectOrdination}>
+                        <option value="">Ordenar</option>
+                        <option value="asc">Crescente</option>
+                        <option value="desc">Decrescente</option>
+                    </select>
+                </div>
             </HomeHeadStyle>            
             <HomeProductsListStyled>
-                {productList.map((product) => {
+                {productList
+                .sort((a,b) => ordination === "" || ordination === "asc" && a.name > b.name ? 1 : -1)
+                .sort((a,b) => ordination === "" || ordination === "desc" && a.name > b.name ? -1 : 1)
+                .map((product) => {
                     return(
                         <ProductCard 
                             name={product.name}
