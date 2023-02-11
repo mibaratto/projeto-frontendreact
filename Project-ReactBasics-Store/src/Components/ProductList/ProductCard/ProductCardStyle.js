8a7f565eddfc063b271425cc8fc39e3838efdc34
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-02-11 02:36:26 +0100

    finish css

--- Project-ReactBasics-Store/src/Components/ProductList/ProductCard/ProductCardStyle.js
+++ Project-ReactBasics-Store/src/Components/ProductList/ProductCard/ProductCardStyle.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
 export const CardStyle = styled.div `
-    width: 200px;
-    height:300px;
-    border: 1px solid black
+    background-color: lightgray;
+    border: 1px solid black;
+    padding: 1rem;
+
+    img {
+        max-width: 200px;
+    }
+
+`
+export const FormatProductText = styled.div `
+    display: flex;
+    flex-direction: column;
+    max-width: 200px;
+    justify-content: space-between;
+    margin-top: 0.5rem;
+    row-gap: 0.5rem;
+
 `
